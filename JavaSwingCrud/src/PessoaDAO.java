9dc8ba55001b4261da7adf1f64351036fc2e0e01
= <=>
2025-06-09 09:31:54 -0300

    Atualiznado

---
+++ JavaSwingCrud/src/PessoaDAO.java
@@ -0,0 +1,41 @@
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Júlio de Souza
+ * @since 09/06/2025;
+ */
+public class PessoaDAO {
+    private Conexao conexao;
+    private Connection conn;
+    
+    public PessoaDAO(){
+        this.conexao = new Conexao();
+        this.conn =  this.conexao.getConexao();
+    }
+    
+    public void inserir (Pessoa pessoa){
+        String sql = "INSERT INTO pessoa (nome, sexo, idioma) VALUES (?,?,?);";
+        
+        try{
+            PreparedStatement stmt = this.conn.prepareStatement(sql);
+            stmt.setString(1, pessoa.getNome());
+            stmt.setString(2, pessoa.getSexo());
+            stmt.setString(3, pessoa.getIdioma());
+            
+            stmt.execute();
+            
+        } catch(SQLException ex){
+            System.out.println("Erro ao inserir pessoa: " + ex.getMessage());
+        }
+    }
+}
---
+++ bdAulaJava.sql
@@ -0,0 +1,10 @@
+create database bdAulaJava;
+
+use bdAulaJava;
+
+create table pessoa(
+ id int NOT null auto_increment primary key,
+ nome varchar(255),
+ sexo varchar(1),
+ idioma varchar(30)
+);
