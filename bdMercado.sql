6786b44a2a63b83fcafb4e23a6b18229f4148d4b
=aluno <=juliodechomp@gmail.com>
2025-06-16 08:34:51 -0300

    Adicionando opcoes de menu

--- bdMercado.sql
+++ bdMercado.sql
@@ -3,17 +3,29 @@ use bdMercado;
 
 
 create table categoria(
-	cat_id int not null auto_increment,
-	cat_nome varchar(25) not null unique,
+	cat_id int not null auto_increment primary key,
+	cat_nome varchar(30) not null,
 	car_descricao varchar(100) 
 );
 
 create table produto(
-	pro_id int not null auto_increment,
+	pro_id int not null auto_increment primary key,
     pro_nome varchar(30) not null,
     pro_preco decimal(6,2) not null,
     pro_codigoBarras varchar(20) not null,
     cat_id int,
     FOREIGN KEY (cat_id) REFERENCES categoria(cat_id)
+);
+
+create table fornecedores(
+	for_CPNJ varchar(50) not null primary key,
+    for_nome varchar(50) not null unique,
+    for_nomeFantasia varchar(50) not null
+);
+
+create table cliente(
+	cli_CPF varchar(50) not null primary key,
+    cli_nome varchar(50) not null,
+    cli_email varchar(50) not null unique
 );
 
